1af8e1d67f9afa7ca9999bdc2bee75287b7155bf
kirito <kirito.kato3253@gmail.com>
2025-12-20 12:29:26 +0900

    fix ABC085C。rangeはデフォルトが0スタートなので不要な部分を削除

--- abc/ABC085C.py
+++ abc/ABC085C.py
@@ -1,6 +1,6 @@
 def check(n, y):
-    for i in range(0, n+1):
-        for j in range(0, n-i+1):
+    for i in range(n+1):
+        for j in range(n-i+1):
             k = n-i-j
             if 10000*i + 5000*j + 1000*k == y:
                 return i, j, k
